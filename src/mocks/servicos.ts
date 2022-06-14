9f4ad30f0a788a94c0fd1d5ef8d229c132c052d0
Bruno Marques <blmarques.dev@gmail.com>
2022-06-14 05:29:56 -0300

    feat(mock-data): mocked data for services

---
+++ src/mocks/servicos.ts
@@ -0,0 +1,45 @@
+export const MOCK_SERVICOS = [
+  {
+    id: '124141265assa2465',
+    title: 'OPORTUNIDADES DE NEGÓCIO',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+    imageUrl: '/images/servicos/busines_opportunities.png'
+  },
+  {
+    id: '165assafdad12465',
+    title: 'OPORTUNIDADES DE NEGÓCIO',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+    imageUrl: '/images/servicos/finance.png'
+  },
+  {
+    id: '124141265assafdad465',
+    title: 'OPORTUNIDADES DE NEGÓCIO',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+    imageUrl: '/images/servicos/it_consulting.png'
+  },
+  {
+    id: 'asdfadfd12465',
+    title: 'OPORTUNIDADES DE NEGÓCIO',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+    imageUrl: '/images/servicos/busines_solution.png'
+  },
+  {
+    id: 'asdf12341265assafdad12',
+    title: 'OPORTUNIDADES DE NEGÓCIO',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+    imageUrl: '/images/servicos/client_management.png'
+  },
+  {
+    id: '1asdf41265assafdad124afd65',
+    title: 'OPORTUNIDADES DE NEGÓCIO',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+    imageUrl: '/images/servicos/online-consulting.png'
+  }
+]
+
+export type ServiceType = {
+    id: number | string;
+    title: string;
+    description: string;
+    imageUrl: string;
+}
