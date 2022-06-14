c3fa93396fc6c1df374d5c347cbcee91b1385d57
Bruno Marques <blmarques.dev@gmail.com>
2022-06-13 21:44:34 -0300

    feat(styled): adding material ui

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true
+  },
+  swcMinify: true,
   reactStrictMode: true
 }
 
---
+++ src/components/header/menu.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function menu () {
+  return (
+    <div>menu</div>
+  )
+}
