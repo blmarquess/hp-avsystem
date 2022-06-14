303cb073898498b14cb005556ce9b939c4fa9424
Bruno Marques <blmarques.dev@gmail.com>
2022-06-14 02:41:45 -0300

    feat(header): created header page

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,30 @@
+import { Box, Grid } from '@mui/material'
+import Image from 'next/image'
+// import  from 'next/link'
+import React from 'react'
+import SimpleLinks from '../Assets/SimpleLinks'
+
+export default function Header () {
+  return (
+    <Grid container alignItems="center" sx={{
+      justifyContent: 'space-between',
+      decoration: 'none',
+      mt: 2
+    }}>
+      <Box>
+        <Box display="flex" alignItems="center">
+              <Image src="/images/logo-header.png" alt="AV System Logo" width={(551 * 0.55)} height={(116 * 0.55)} />
+        </Box>
+      </Box>
+      <Box display="flex" alignItems="center" sx={{ textDecoration: 'none' }}>
+        <SimpleLinks href="https://www.avsystem.com/">HOME</SimpleLinks>
+        <SimpleLinks href="https://www.avsystem.com/">QUEM SOMOS</SimpleLinks>
+        <SimpleLinks href="https://www.avsystem.com/">SERVIÇOS</SimpleLinks>
+        <SimpleLinks href="https://www.avsystem.com/">CLIENTES</SimpleLinks>
+        <SimpleLinks href="https://www.avsystem.com/">CASES</SimpleLinks>
+        <SimpleLinks href="https://www.avsystem.com/">SISTEMAS</SimpleLinks>
+        <SimpleLinks href="https://www.avsystem.com/">CONTATO</SimpleLinks>
+      </Box>
+    </Grid>
+  )
+}
--- src/components/header/menu.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function menu () {
-  return (
-    <div>menu</div>
-  )
-}
